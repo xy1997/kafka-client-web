10a8522bc3af92bcbc900bcdd762116452b4006c
xingyu <xingyu@hylink.net.cn>
2024-10-25 17:01:43 +0800

    table添加查询broker信息的功能

--- src/api/kafka/cluster.ts
+++ src/api/kafka/cluster.ts
@@ -1,17 +1,17 @@
-import service from '@/utils/request'
+import service from "@/utils/request";
 
 export const addCluster = (data: any) => {
-    return service.post('/kafka/broker/add', data)
-}
+  return service.post("/kafka/broker/add", data);
+};
 
-export const describeCluster = () => {
-    return service.get('/kafka/broker/describeCluster')
-}
+export const describeCluster = (data: any) => {
+  return service.get("/kafka/broker/describeCluster?brokerId="+data);
+};
 
 export const update = (data: any) => {
-    return service.post('/kafka/broker/update', data)
-}
+  return service.post("/kafka/broker/update", data);
+};
 
 export const detail = (data: any) => {
-    return service.get('/kafka/broker/detail?id=' + data)
-}
+  return service.get("/kafka/broker/detail?id=" + data);
+};
--- src/views/cluster/index.vue
+++ src/views/cluster/index.vue
@@ -1,66 +1,112 @@
 <template>
-	<el-card>
-		<el-form :inline="true" :model="state.queryForm" @keyup.enter="getDataList()">
-			<el-form-item>
-				<el-input v-model="state.queryForm.broker" placeholder="cluster server" clearable></el-input>
-			</el-form-item>
-			<el-form-item>
-				<el-button @click="getDataList()">查询</el-button>
-			</el-form-item>
-			<el-form-item>
-				<el-button type="primary" @click="addOrUpdateHandle()">新增</el-button>
-			</el-form-item>
-			<el-form-item>
-				<el-button type="danger" @click="deleteBatchHandle()">删除</el-button>
-			</el-form-item>
-		</el-form>
-		<el-table v-loading="state.dataListLoading" :data="state.dataList" border style="width: 100%" @selection-change="selectionChangeHandle">
-			<el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
-			<el-table-column prop="name" label="cluster name" header-align="center" align="center"  width="300"></el-table-column>
-			<el-table-column prop="broker" label="cluster server" header-align="center" align="center" show-overflow-tooltip></el-table-column>
-			<el-table-column prop="createdTime" label="创建时间" header-align="center" align="center" width="180"></el-table-column>
-			<el-table-column label="操作" fixed="right" header-align="center" align="center" width="150">
-				<template #default="scope">
-					<el-button type="primary" link @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
-					<el-button type="primary" link @click="deleteBatchHandle(scope.row.id)">删除</el-button>
-				</template>
-			</el-table-column>
-		</el-table>
-		<el-pagination
-			:current-page="state.page"
-			:page-sizes="state.pageSizes"
-			:page-size="state.limit"
-			:total="state.total"
-			layout="total, sizes, prev, pager, next, jumper"
-			@size-change="sizeChangeHandle"
-			@current-change="currentChangeHandle"
-		>
-		</el-pagination>
+  <el-card>
+    <el-form :inline="true" :model="state.queryForm" @keyup.enter="getDataList()">
+      <el-form-item>
+        <el-input v-model="state.queryForm.broker" placeholder="cluster server" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="getDataList()">查询</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="addOrUpdateHandle()">新增</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="danger" @click="deleteBatchHandle()">删除</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table v-loading="state.dataListLoading" :data="state.dataList" border style="width: 100%"
+              @selection-change="selectionChangeHandle">
+      <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
+      <el-table-column prop="name" label="cluster name" header-align="center" align="center"
+                       width="300"></el-table-column>
+      <el-table-column prop="broker" label="cluster server" header-align="center" align="center"
+                       show-overflow-tooltip></el-table-column>
+      <el-table-column label="brokers" header-align="center" align="center">
+        <template #default="scope">
 
-		<!-- 弹窗, 新增 / 修改 -->
-		<add-or-update ref="addOrUpdateRef" @refreshDataList="getDataList"></add-or-update>
-	</el-card>
+
+          <el-popover placement="right" :width="460" trigger="click" @show="showBrokers(scope.row.id)">
+            <template #reference>
+              <el-icon size="20">
+                <InfoFilled />
+              </el-icon>
+            </template>
+            <el-table :data="brokersData" v-loading="brokerLoading">
+              <el-table-column width="150" property="host" label="host" />
+              <el-table-column width="150" property="port" label="port" />
+              <el-table-column width="150" property="controller" label="controller" />
+            </el-table>
+          </el-popover>
+        </template>
+
+
+      </el-table-column>
+
+      <el-table-column prop="createdTime" label="创建时间" header-align="center" align="center"
+                       width="180"></el-table-column>
+      <el-table-column label="操作" fixed="right" header-align="center" align="center" width="150">
+        <template #default="scope">
+          <el-button type="primary" link @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+          <el-button type="primary" link @click="deleteBatchHandle(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      :current-page="state.page"
+      :page-sizes="state.pageSizes"
+      :page-size="state.limit"
+      :total="state.total"
+      layout="total, sizes, prev, pager, next, jumper"
+      @size-change="sizeChangeHandle"
+      @current-change="currentChangeHandle"
+    >
+    </el-pagination>
+
+    <!-- 弹窗, 新增 / 修改 -->
+    <add-or-update ref="addOrUpdateRef" @refreshDataList="getDataList"></add-or-update>
+  </el-card>
 </template>
 
 <script setup lang="ts">
-import { useCrud } from '@/hooks'
-import { reactive, ref } from 'vue'
-import AddOrUpdate from './add-or-update.vue'
-import { IHooksOptions } from '@/hooks/interface'
+import { useCrud } from "@/hooks";
+import { reactive, ref } from "vue";
+import AddOrUpdate from "./add-or-update.vue";
+import { IHooksOptions } from "@/hooks/interface";
+import { InfoFilled } from "@element-plus/icons-vue";
+import { describeCluster } from "@/api/kafka/cluster";
 
 const state: IHooksOptions = reactive({
-	dataListUrl: '/kafka/broker/searchPage',
-	deleteUrl: '/kafka/broker/deleteBatch'
-	// queryForm: {
-	// 	username: '',
-	// 	mobile: ''
-	// }
-})
+  dataListUrl: "/kafka/broker/searchPage",
+  deleteUrl: "/kafka/broker/deleteBatch"
+  // queryForm: {
+  // 	username: '',
+  // 	mobile: ''
+  // }
+});
 
-const addOrUpdateRef = ref()
+const addOrUpdateRef = ref();
+//broker 数据
+const brokersData = ref([]);
+const brokerLoading = ref();
+
 const addOrUpdateHandle = (id?: number) => {
-	addOrUpdateRef.value.init(id)
-}
+  addOrUpdateRef.value.init(id);
+};
 
-const { getDataList, selectionChangeHandle, sizeChangeHandle, currentChangeHandle, deleteBatchHandle } = useCrud(state)
+const showBrokers = async (id: any) => {
+  brokerLoading.value = true;
+
+  if (brokersData.value) {
+    brokersData.value = [];
+  }
+
+  try {
+    const { data } = await describeCluster(id);
+    brokersData.value = data.nodes;
+  } finally {
+    brokerLoading.value = false;
+  }
+};
+
+const { getDataList, selectionChangeHandle, sizeChangeHandle, currentChangeHandle, deleteBatchHandle } = useCrud(state);
 </script>
