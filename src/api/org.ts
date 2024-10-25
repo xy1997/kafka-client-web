6580d9eb62c04b7077cc9ca42e96c32be5b57125
xingyu <xingyu@hylink.net.cn>
2024-10-25 15:49:26 +0800

    删除

--- src/api/org.ts
+++
@@ -1,9 +0,0 @@
-import service from '@/utils/request'
-
-export const useOrgListApi = () => {
-	return service.get('/data/org/list.json')
-}
-
-export const useOrgApi = (id: Number) => {
-	return service.get('/data/org/1.json')
-}
--- src/utils/cache/key.ts
+++
@@ -1,7 +0,0 @@
-export default {
-	SidebarOpenedKey: 'sidebarOpened',
-	LangKey: 'lang',
-	ComponentSizeKey: 'componentSize',
-	TokenKey: 'token',
-	ThemeKey: 'theme'
-}
